417206e17cee8e337b4cea95ed89a0875b71c653
dkmyta <dkmyta10@gmail.com>
2022-09-13 15:57:19 -0700

    Remove unnecessary variables and comments for the time being

--- projects/plugins/protect/src/js/components/interstitial/index.jsx
+++ projects/plugins/protect/src/js/components/interstitial/index.jsx
@@ -1,7 +1,4 @@
 import {
-	// Dialog,
-	// ProductOffer,
-	// useBreakpointMatch,
 	Button,
 	ProductPrice,
 	PricingTable,
@@ -9,54 +6,9 @@ import {
 	PricingTableHeader,
 	PricingTableItem,
 } from '@automattic/jetpack-components';
-// import { ToS } from '@automattic/jetpack-connection';
+//import { ToS } from '@automattic/jetpack-connection';
 import React from 'react';
 // import useProtectData from '../../hooks/use-protect-data';
-// import ConnectedProductOffer from '../product-offer';
-// import styles from './styles.module.scss';
-
-// const SecurityBundle = ( { onAdd, redirecting, ...rest } ) => {
-// 	const { securityBundle } = useProtectData();
-// 	const {
-// 		name,
-// 		title,
-// 		longDescription,
-// 		isBundle,
-// 		supportedProducts,
-// 		features,
-// 		pricingForUi,
-// 	} = securityBundle;
-
-// 	// Compute the price per month.
-// 	const price = pricingForUi.fullPrice
-// 		? Math.ceil( ( pricingForUi.fullPrice / 12 ) * 100 ) / 100
-// 		: null;
-// 	const offPrice = pricingForUi.discountPrice
-// 		? Math.ceil( ( pricingForUi.discountPrice / 12 ) * 100 ) / 100
-// 		: null;
-// 	const { currencyCode: currency = 'USD' } = pricingForUi;
-
-// 	return (
-// 		<ProductOffer
-// 			slug="security"
-// 			name={ name }
-// 			title={ title }
-// 			description={ longDescription }
-// 			isBundle={ isBundle }
-// 			supportedProducts={ supportedProducts }
-// 			features={ features }
-// 			pricing={ {
-// 				currency,
-// 				price,
-// 				offPrice,
-// 			} }
-// 			hasRequiredPlan={ false }
-// 			onAdd={ onAdd }
-// 			isLoading={ redirecting }
-// 			{ ...rest }
-// 		/>
-// 	);
-// };
 
 /**
  * Intersitial Protect component.
@@ -64,12 +16,6 @@ import React from 'react';
  * @returns {React.Component} Interstitial react component.
  */
 const Interstitial = () => {
-	// const Interstitial = ( { onSecurityAdd, securityJustAdded } ) => {
-	// const [ isMediumSize ] = useBreakpointMatch( 'md' );
-	// const mediaClassName = `${ styles.section } ${
-	// 	isMediumSize ? styles[ 'is-viewport-medium' ] : ''
-	// }`;
-
 	const args = {
 		title: 'Stay one step ahead of threats',
 		items: [
@@ -109,7 +55,7 @@ const Interstitial = () => {
 			</PricingTableColumn>
 			<PricingTableColumn>
 				<PricingTableHeader>
-					<ProductPrice price={ 0 } leyend={ '\u00A0' } currency="USD" hidePriceFraction />
+					<ProductPrice price={ 0 } leyend="Free forever" currency="USD" hidePriceFraction />
 					<Button fullWidth variant="secondary">
 						Start for free
 					</Button>
