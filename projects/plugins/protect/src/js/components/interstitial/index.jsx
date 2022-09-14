09f00cb867870d48bd649047b9f18196d73145ab
dkmyta <dkmyta10@gmail.com>
2022-09-14 15:28:30 -0700

    Add Protect pricingTable component, display component on interstitial page, introduce a savings label within ProductPrice component, add ToS component, and add Free selection functionality

--- projects/plugins/protect/src/js/components/interstitial/index.jsx
+++ projects/plugins/protect/src/js/components/interstitial/index.jsx
@@ -1,14 +1,5 @@
-import {
-	Button,
-	ProductPrice,
-	PricingTable,
-	PricingTableColumn,
-	PricingTableHeader,
-	PricingTableItem,
-} from '@automattic/jetpack-components';
-//import { ToS } from '@automattic/jetpack-connection';
 import React from 'react';
-// import useProtectData from '../../hooks/use-protect-data';
+import ConnectedPricingTable from '../pricing-table';
 
 /**
  * Intersitial Protect component.
@@ -16,59 +7,7 @@ import React from 'react';
  * @returns {React.Component} Interstitial react component.
  */
 const Interstitial = () => {
-	const args = {
-		title: 'Stay one step ahead of threats',
-		items: [
-			'Scan for threats and vulnerabilities',
-			'Daily automated scans',
-			'Access to scan on Cloud',
-			'One-click auto fixes',
-			'Notifications',
-			'Severity labels',
-		],
-	};
-
-	return (
-		<PricingTable { ...args }>
-			<PricingTableColumn>
-				<PricingTableHeader>
-					<ProductPrice
-						price={ 9.95 }
-						offPrice={ 4.98 }
-						leyend="Starting price per month, billed yearly"
-						currency="USD"
-					/>
-					<Button fullWidth>Get Jetpack Protect</Button>
-				</PricingTableHeader>
-				<PricingTableItem
-					isIncluded={ true }
-					label={ <strong>Line by line malware scanning</strong> }
-				/>
-				<PricingTableItem
-					isIncluded={ true }
-					label={ <strong>Plus on-demand manual scans</strong> }
-				/>
-				<PricingTableItem isIncluded={ true } />
-				<PricingTableItem isIncluded={ true } />
-				<PricingTableItem isIncluded={ true } />
-				<PricingTableItem isIncluded={ true } />
-			</PricingTableColumn>
-			<PricingTableColumn>
-				<PricingTableHeader>
-					<ProductPrice price={ 0 } leyend="Free forever" currency="USD" hidePriceFraction />
-					<Button fullWidth variant="secondary">
-						Start for free
-					</Button>
-				</PricingTableHeader>
-				<PricingTableItem isIncluded={ true } label="Check items against database" />
-				<PricingTableItem isIncluded={ true } />
-				<PricingTableItem isIncluded={ false } />
-				<PricingTableItem isIncluded={ false } />
-				<PricingTableItem isIncluded={ false } />
-				<PricingTableItem isIncluded={ false } />
-			</PricingTableColumn>
-		</PricingTable>
-	);
+	return <ConnectedPricingTable />;
 };
 
 export default Interstitial;
---
+++ projects/plugins/protect/src/js/components/pricing-table/index.jsx
@@ -0,0 +1,135 @@
+/**
+ * External dependencies
+ */
+import {
+	Button,
+	ProductPrice,
+	PricingTable,
+	PricingTableColumn,
+	PricingTableHeader,
+	PricingTableItem,
+} from '@automattic/jetpack-components';
+import { useConnection, ToS } from '@automattic/jetpack-connection';
+import { __ } from '@wordpress/i18n';
+import PropTypes from 'prop-types';
+import React, { useCallback } from 'react';
+import styles from './styles.module.scss';
+// import useAnalyticsTracks from '../../hooks/use-analytics-tracks';
+
+/**
+ * Product Detail component.
+ * ToDo: rename event handler properties.
+ *
+ * @param {object} props              - Component props.
+ * @param {Function} props.onAdd      - Callback when adding protect product successfully
+ * @param {Function} props.onAddError - Callback when adding protect product fails
+ * @returns {object}                    ConnectedPricingTable react component.
+ */
+const ConnectedPricingTable = ( { onAdd, onAddError } ) => {
+	const args = {
+		title: __( 'Stay one step ahead of threats', 'jetpack-protect' ),
+		items: [
+			__( 'Scan for threats and vulnerabilities', 'jetpack-protect' ),
+			__( 'Daily automated scans', 'jetpack-protect' ),
+			__( 'Access to scan on Cloud', 'jetpack-protect' ),
+			__( 'One-click auto fixes', 'jetpack-protect' ),
+			__( 'Notifications', 'jetpack-protect' ),
+			__( 'Severity labels', 'jetpack-protect' ),
+		],
+	};
+
+	const { siteIsRegistering, handleRegisterSite, registrationError } = useConnection( {
+		skipUserConnection: true,
+	} );
+
+	// 	const { recordEvent } = useAnalyticsTracks();
+
+	const onAddHandler = useCallback( () => {
+		// Record event in case the site did register.
+		return handleRegisterSite()
+			.then( () => {
+				// recordEvent( 'jetpack_protect_offer_connect_product_activated' );
+				onAdd();
+			} )
+			.catch( err => {
+				onAddError( err );
+			} );
+	}, [ handleRegisterSite, onAdd, onAddError ] ); // recordEvent ] );
+
+	return (
+		<>
+			<PricingTable { ...args }>
+				<PricingTableColumn>
+					<PricingTableHeader>
+						<ProductPrice
+							price={ 9.95 }
+							offPrice={ 4.98 }
+							leyend={ __( '/month, billed yearly', 'jetpack-protect' ) }
+							currency="USD"
+						/>
+						<Button fullWidth>{ __( 'Get Jetpack Protect', 'jetpack-protect' ) }</Button>
+					</PricingTableHeader>
+					<PricingTableItem
+						isIncluded={ true }
+						label={ <strong>{ __( 'Line by line malware scanning', 'jetpack-protect' ) }</strong> }
+					/>
+					<PricingTableItem
+						isIncluded={ true }
+						label={ <strong>{ __( 'Plus on-demand manual scans', 'jetpack-protect' ) }</strong> }
+					/>
+					<PricingTableItem isIncluded={ true } />
+					<PricingTableItem isIncluded={ true } />
+					<PricingTableItem isIncluded={ true } />
+					<PricingTableItem isIncluded={ true } />
+				</PricingTableColumn>
+				<PricingTableColumn>
+					<PricingTableHeader>
+						<ProductPrice
+							price={ 0 }
+							leyend={ __( 'Free forever', 'jetpack-protect' ) }
+							currency="USD"
+							hidePriceFraction
+						/>
+						<Button
+							fullWidth
+							variant="secondary"
+							onClick={ onAddHandler }
+							isLoading={ siteIsRegistering }
+							error={
+								registrationError
+									? __( 'An error occurred. Please try again.', 'jetpack-protect' )
+									: null
+							}
+						>
+							{ __( 'Start for free', 'jetpack-protect' ) }
+						</Button>
+					</PricingTableHeader>
+					<PricingTableItem
+						isIncluded={ true }
+						label={ __( 'Check items against database', 'jetpack-protect' ) }
+					/>
+					<PricingTableItem isIncluded={ true } />
+					<PricingTableItem isIncluded={ false } />
+					<PricingTableItem isIncluded={ false } />
+					<PricingTableItem isIncluded={ false } />
+					<PricingTableItem isIncluded={ false } />
+				</PricingTableColumn>
+			</PricingTable>
+			<p className={ styles.tos }>{ ToS }</p>
+		</>
+	);
+};
+
+ConnectedPricingTable.propTypes = {
+	/** props.onAdd      - Callback when adding protect product successfully */
+	onAdd: PropTypes.func,
+	/** props.onError    - Callback when adding protect product fails */
+	onAddError: PropTypes.func,
+};
+
+ConnectedPricingTable.defaultProps = {
+	onAdd: () => {},
+	onAddError: () => {},
+};
+
+export default ConnectedPricingTable;
